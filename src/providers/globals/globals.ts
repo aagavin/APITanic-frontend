fcea8f0258c4623c798e5bac21b5e5876c89fc03
aagavin <aagavin@gmail.com>
2017-12-31 22:52:55 -0500

    working CORS and pre flight

--- src/providers/globals/globals.ts
+++ src/providers/globals/globals.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalsProvider {
 
-  public static readonly BASEURL:string = 'https://aaronfernandescc-eval-prod.apigee.net';
+  public static readonly BASEURL:string = 'https://aaronfernandescc-eval-prod.apigee.net/apitanic';
   
   constructor() { console.log('Hello GlobalsProvider Provider'); }
 
