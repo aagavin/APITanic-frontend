354972621d25196f532498698a27ff3edb8a7b3a
aagavin <aagavin@gmail.com>
2017-12-31 22:52:15 -0500

    sign in page

---
+++ src/pages/signin-pages/signin/signin.ts
@@ -0,0 +1,38 @@
+import {UserProvider} from '../../../providers/user/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-signin',
+  templateUrl: 'signin.html',
+})
+export class SigninPage {
+
+  public signInFrom: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private _formBuilder: FormBuilder,
+    private _userProvider: UserProvider
+  ) {
+    this.signInFrom = this._formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, ]],
+    });
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SigninPage');
+  }
+
+
+  public onSubmit(){
+    console.log(this.signInFrom.value);
+    this._userProvider.signIn(this.signInFrom.value.email, this.signInFrom.value.password);
+  }
+
+}
