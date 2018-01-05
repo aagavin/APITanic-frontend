1aec0507436f072701d22403bd20efb7c405ef29
aagavin <aagavin@gmail.com>
2018-01-04 23:35:13 -0500

    userservice

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -5,24 +5,56 @@ import { GlobalsProvider } from '../globals/globals';
 import { Storage } from '@ionic/storage';
 
 import { AngularFireAuth } from 'angularfire2/auth';
+// needed for tree shakeing
 import * as firebase from 'firebase/app';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
 export class UserProvider {
 
+  private isLoggedIn: Subject<boolean>;
+  private token: String;
+
   constructor(public http: HttpClient, private _storage: Storage, private _afAuth: AngularFireAuth) {
     console.log('Hello UserProvider Provider');
+    this.isLoggedIn = new Subject<boolean>();
+    this.isLoggedIn.next(false);
+    this._afAuth.authState.subscribe(async (user: firebase.User) =>{
+      try {
+        this.token = await user.getToken();
+        this.isLoggedIn.next(true);
+      }
+      catch (e) {
+        this.isLoggedIn.next(false);
+        this.token = '';
+      }
+    });
   }
 
-/**
- * Creats a new user
- * 
- * @param {SignUpUser} signUpUser 
- * @returns {Promise<boolean>} 
- * @memberof UserProvider
- */
-public async createUser(signUpUser: SignUpUser):Promise<boolean> {
+
+  /**
+   * Getter for user token
+   * 
+   * @readonly
+   * @type {String}
+   * @memberof UserProvider
+   */
+  public get $token(): String { return this.token; }
+
+  public get $isLoggedIn(): Observable<boolean> {
+		return this.isLoggedIn.asObservable();
+	}
+
+  /**
+   * Creats a new user
+   * 
+   * @param {SignUpUser} signUpUser 
+   * @returns {Promise<boolean>} 
+   * @memberof UserProvider
+   */
+  public async createUser(signUpUser: SignUpUser): Promise<boolean> {
     let payload = {
       "password": signUpUser.password,
       "displayName": signUpUser.displayName,
@@ -44,10 +76,14 @@ public async createUser(signUpUser: SignUpUser):Promise<boolean> {
 
   }
 
-  public signIn(email: string, password: string){
-    this._afAuth.auth.signInWithEmailAndPassword(email, password)
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+  public async signIn(email: string, password: string): Promise<any> {
+    let data = await this._afAuth.auth.signInWithEmailAndPassword(email, password)
   }
+
+  public async signOut(): Promise<any> {
+    let a = this._afAuth.auth.signOut();
+  }
+
+  
 
 }
