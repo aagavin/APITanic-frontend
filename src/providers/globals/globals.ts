38b5d97745bff90768ebe406d0e3e2a144664e85
aagavin <aagavin@gmail.com>
2018-01-07 13:03:39 -0500

    global provider

--- src/providers/globals/globals.ts
+++ src/providers/globals/globals.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 export class GlobalsProvider {
 
   public static readonly BASEURL:string = 'https://aaronfernandescc-eval-prod.apigee.net/apitanic';
+  // public static readonly BASEURL:string = 'http://127.0.0.1:8000';
   
   constructor() { console.log('Hello GlobalsProvider Provider'); }
 
