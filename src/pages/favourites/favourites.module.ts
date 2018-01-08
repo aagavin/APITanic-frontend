e8ec72f06e57212d3a2ebfd4e8fe81f2a62fe96f
aagavin <aagavin@gmail.com>
2018-01-07 23:20:35 -0500

    pipesModule

--- src/pages/favourites/favourites.module.ts
+++ src/pages/favourites/favourites.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { FavouritesPage } from './favourites';
 
+import { ImdbProvider } from '../../providers/imdb/imdb';
+import { PipesModule } from "../../pipes/pipes.module";
+
+
 @NgModule({
   declarations: [
     FavouritesPage,
   ],
   imports: [
     IonicPageModule.forChild(FavouritesPage),
+    PipesModule
   ],
+  providers: [ImdbProvider]
 })
 export class FavouritesPageModule {}
