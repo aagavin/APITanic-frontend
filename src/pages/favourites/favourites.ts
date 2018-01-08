54ee3940b8c8e48dad63645a3870b5a55b746ef8
aagavin <aagavin@gmail.com>
2018-01-07 23:20:23 -0500

    favourites

--- src/pages/favourites/favourites.ts
+++ src/pages/favourites/favourites.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { forkJoin } from "rxjs/observable/forkJoin";
+import { ImdbProvider } from '../../providers/imdb/imdb';
+import { UserProvider } from '../../providers/user/user';
 
 @IonicPage()
 @Component({
@@ -8,11 +12,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FavouritesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public favourites: Array<any>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private userProvider: UserProvider, private imdbProvider: ImdbProvider) {
+    this.favourites = new Array<object>();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FavouritesPage');
+  async ionViewDidLoad() {
+    this.getFavourites();
   }
+
+  private getFavourites(){
+    this.userProvider.getAllFavourites().subscribe(
+      response => this.getMovieData(response['data']['favourites']),
+      err => console.log(err),
+      () => console.log('done getting logs')
+    );
+  }
+
+  public removeFavourite(imdbid: string) {
+    console.log(imdbid);
+
+    this.userProvider.removeFavourite(imdbid).subscribe(
+      response => console.log(response),
+      err => console.log(err),
+      ()=> {console.log('done removeing favourite'); this.getFavourites()}
+    )
+
+  }
+
+  /**
+   * Gets movie data from a list of favourites
+   * 
+   * @private
+   * @param {any} favourties 
+   * @memberof FavouritesPage
+   */
+  private getMovieData(favourties) {
+
+    let observableList = [];
+
+    favourties.forEach(value => observableList.push(this.imdbProvider.getMovieById(value.imdbid)));
+
+    forkJoin(observableList).subscribe(results => {
+      this.favourites = results.map(result => result['data']['movie'])
+    });
+  }
+
 
 }
