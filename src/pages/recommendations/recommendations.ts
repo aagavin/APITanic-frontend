65c7dda75d7d217f5bf966de038e77cbf058d42f
aagavin <aagavin@gmail.com>
2018-01-13 19:48:53 -0500

    recommendations ts

--- src/pages/recommendations/recommendations.ts
+++ src/pages/recommendations/recommendations.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ToastController, LoadingController, IonicPage, NavController, NavParams } from 'ionic-angular';
+import { forkJoin } from "rxjs/observable/forkJoin";
+import { UserProvider } from '../../providers/user/user';
+import { ImdbProvider } from '../../providers/imdb/imdb';
 
 @IonicPage()
 @Component({
@@ -8,11 +11,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RecommendationsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private recommendationsIds: Array<string>;
+  public recommendations: Array<object>;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private userProvider: UserProvider,
+    private imdbProvider: ImdbProvider,
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+  ) {
+    this.recommendationsIds = new Array<string>();
+    this.getRecommendations();
+  }
+
+  private getRecommendations(){
+    this.userProvider.getRecommendations().subscribe(
+      results => this.recommendationsIds = results['data']['recommendations'],
+      err => console.log(err),
+      () => this.getMovieInfo()
+    );
+  }
+
+  private getMovieInfo() {
+    let observableList = this.recommendationsIds.map((value: string) => {
+      return this.imdbProvider.getMovieById(value)
+    });
+
+    forkJoin(observableList).subscribe(results => {
+      this.recommendations = results.map(result => result['data']['movie'])
+    });
+
+  }
+
+  private sendToast(message: string) {
+    this.toastController.create({
+      message: message,
+      duration: 3000
+    }).present()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecommendationsPage');
+  }
+
+  public addFavourite(imdbId: string) {
+    let loading = this.loadingController.create({
+      content: `Adding a favourite`
+    });
+    loading.present();
+
+    console.log(imdbId);
+
+    this.userProvider.addToFavourites(imdbId).subscribe(
+      response => {
+        if (!response['data']['success']) {
+          this.sendToast(response['data']['message'])
+        }
+        else {
+          this.sendToast('Favourite added')
+        }
+      },
+      err => console.log(err),
+      () => {loading.dismiss();this.getRecommendations()}
+    );
   }
 
 }
